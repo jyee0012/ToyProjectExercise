128f379cec2a5ed645facefc4444c16d65a951bb
welsan21 <35718163+welsan21@users.noreply.github.com>
2018-10-24 14:54:56 -0600

    MainMenu

---
+++ ToyProjectExercise/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject pausePanel;
+    bool paused;
+    private void Start()
+    {
+        paused = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+        if (paused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            //Cursor.visible = true;
+            //Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1;
+            //Cursor.visible = false;
+            //Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+    public void PauseGame()
+    {
+        paused = !paused;
+    }
+    public void ReturnToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume ()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+}
