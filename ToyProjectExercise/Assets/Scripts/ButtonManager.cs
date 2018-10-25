d8c579fa466b0f31cc59502f5a9c3a2d7996b864
jyee0012 <jyee12@studentmail.nait.ca>
2018-10-25 16:52:03 -0600

    This is the Final Game Doo doo doo

--- ToyProjectExercise/Assets/Scripts/ButtonManager.cs
+++ ToyProjectExercise/Assets/Scripts/ButtonManager.cs
@@ -23,11 +23,8 @@ public class ButtonManager : MonoBehaviour {
 
     void Update()
     {
-<<<<<<< HEAD
+
         if (SceneManager.GetActiveScene().name == "ResultsScreen")
-=======
-        if (SceneManager.GetActiveScene().name == "ResultsScene")
->>>>>>> 640905d5e6c6a3aef2cc57d0b4bce87db093729d
         {
             if (!ScoreStatics.p2Active) scoreText.GetComponent<Text>().text = "Your Score: " + ScoreStatics.p1Score.ToString();
             else scoreText.GetComponent<Text>().text = "Player 1 Score: " + ScoreStatics.p1Score.ToString() + "\nPlayer 2 Score: " + ScoreStatics.p2Score.ToString();
